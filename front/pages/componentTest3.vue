744dd4628ffc8f96f45631d42d01fdf2f5128f71
Temmy3620 <me512papiko512@gmail.com>
2024-04-06 00:46:53 +0900

    多言語化切り替え

--- front/pages/componentTest3.vue
+++ front/pages/componentTest3.vue
@@ -1,12 +1,34 @@
 <template>
 
-    <div>{{ $t('hello') }} </div>
+    <v-container>
+
+        <h1>{{ $t('hello') }} </h1>
+        
+        <v-row>
+            <v-col cols="3">
+                <v-select
+                label="Select"
+                v-model="$i18n.locale"
+                :items="languages"
+                item-title="title"
+                item-value="value"
+                variant="outlined"
+                class="mt-5"
+                ></v-select>
+            </v-col>
+        </v-row>
+    </v-container>
+    
     
     
     
 </template>
 <script setup lang="ts">
     const message = 'Component Test !!';
+    const languages = [
+      { title: 'English', value: 'en' },
+      { title: '日本語', value: 'ja' },
+    ];
 
     console.log("--------------------------------");
     console.log(message);
